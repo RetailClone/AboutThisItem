7114b64c3fe98e8e5064a90baa3b14b6b279888b
andrewbinkard <andrewbinkard@gmail.com>
2020-08-06 15:20:32 -0500

    front end renders

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const db = require('../Database/database');
-const app = express();
 const PORT = 1701;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../Public'));
+
 
 app.listen(PORT, () => {
+  console.log("DIR", __dirname);
   console.log(`listening on port ${PORT}`);
 });
