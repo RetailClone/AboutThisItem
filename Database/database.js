c0cf5f53e402f94351899d3a029ebe173a0d6875
andrewbinkard <andrewbinkard@gmail.com>
2020-08-08 19:08:54 -0500

    database crisis averted

--- Database/database.js
+++ Database/database.js
@@ -1,55 +1,56 @@
-const mysql = require('mysql');
-const mysqlConfig = require('./dbconfig.js');
+const mysql = require("mysql");
+const mysqlConfig = require("./dbconfig.js");
 const connection = mysql.createConnection(mysqlConfig);
 
 // get the description
 const getDesc = (id, cb) => {
-  const queryString = 'SELECT item_description FROM items WHERE id = ?';
+  const queryString = "SELECT item_description FROM items WHERE id = ?";
   connection.query(queryString, [id], (err, results) => {
     if (err) {
       cb(err, null);
       console.error("Could not get description from DB - database.js line 30");
       return;
     } else {
-      cb(null, results)
+      cb(null, results);
     }
-  })
-}
+  });
+};
 
 //get specs
 const getSpecs = (id, cb) => {
-  const queryString = 'SELECT specs FROM item_specs WHERE item_id = ?';
+  const queryString = "SELECT specs FROM item_specs WHERE item_id = ?";
   connection.query(queryString, [id], (err, results) => {
     if (err) {
       cb(err, null);
       console.error("Could not get specs from DB - database.js line 44");
       return;
     } else {
-      cb(null, results)
+      cb(null, results);
     }
-  })
-}
+  });
+};
 
 //get highlights
 const getHighlights = (id, cb) => {
-  const queryString = 'SELECT highlights FROM item_highlights WHERE item_id = ?';
+  const queryString =
+    "SELECT highlights FROM item_highlights WHERE item_id = ?";
   connection.query(queryString, [id], (err, results) => {
     if (err) {
       cb(err, null);
       console.error("Could not get highlights from DB - database.js line 58");
       return;
     } else {
-      cb(null, results)
+      cb(null, results);
     }
-  })
-}
+  });
+};
 
-connection.connect((err => {
+connection.connect((err) => {
   if (err) {
     console.log(`Couldn't connect to database`, err);
   } else {
     console.log(`Connected to mySQL database`);
   }
-}));
+});
 
-module.exports = { getDesc, getSpecs, getHighlights }
+module.exports = { getDesc, getSpecs, getHighlights };
